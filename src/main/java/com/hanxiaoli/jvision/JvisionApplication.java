e606843757d2b6766c920133031a36bad20a625f
hanxiaolli <hanxiaolc@gmail.com>
2018-07-01 10:53:22 +0900

    commit

---
+++ src/main/java/com/hanxiaoli/jvision/JvisionApplication.java
@@ -0,0 +1,12 @@
+package com.hanxiaoli.jvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvisionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hanxiaoli/jvision/JvisionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hanxiaoli.jvision;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JvisionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
